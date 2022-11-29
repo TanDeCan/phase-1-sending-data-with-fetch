ecfba6f3de00289b4fc5e52820c099832a1f7540
VictoriaBladow <vbladow9999@yahoo.com>
2022-11-28 20:31:15 -0600

    commit tree 32ddb73cb0872c30322585a316b2e2a3983f53b8

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email){
+
+const addData = {
+    method: "POST",
+    headers: {
+       "Content-Type" : "application/json",
+        "Accept" : "application/json",
+    },
+    body: JSON.stringify({
+        name: name,
+        email: email,
+    })
+};
+
+return fetch("http://localhost:3000/users", addData)
+    .then((resp) => resp.json())
+    .then ((data) => {
+        document.body.innerHTML = data.id
+        console.log('data',data)
+    })
+    .catch(function (error) {alert("AAAHHHHH! HELP!!")
+    document.body.innerHTML = error.message
+    console.log(error.message);
+})};
